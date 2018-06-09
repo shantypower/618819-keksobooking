4c3f4cff31916e056f68f4af5133aeb160c08d6e
shantypower <shantypower4891@yandex.ru>
2018-06-09 18:40:49 +1000

    Частично написана функция генерации объекта - одного оуникального объявления

--- js/map.js
+++ js/map.js
@@ -1,38 +1,121 @@
 'use strict';
 
 var getRandomNumber = function (min, max) {
-  var randomNumber = Math.ceil(Math.random() * (max - min) + min);
+  var randomNumber = Math.round(Math.random() * (max - min) + min);
   return randomNumber;
 }
 
-var getRandomArrowElement = function (arr) {
-  var randomElementIndex = Math.floor(Math.random() * arr.length);
+var getRandomArrayElement = function (arr) {
+  var randomElementIndex = getRandomNumber(0, arr.length - 1);
   return arr[randomElementIndex];
 };
+/*
+var getUniqueArrayElement = function (arr) {
+  var uniqueArrayElement = getRandomArrayElement(arr);
+  arr.slice
+}
+console.log(getUniqueArrayElement(offerTitles));*/
 
-var authorAvatars = 'img/avatars/user' + '0' + getRandomNumber(0, 8) + '.png'; //строка, адрес изображения вида img/avatars/user{{xx}}.png, где {{xx}} это число от 1 до 8 с ведущим нулём. Например, 01, 02 и т. д. Адреса изображений не повторяются
-var offerTitles = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде']; //Значения не должны повторяться.
-var offerAddress = ''+ ', ' + '';  //строка, адрес предложения, представляет собой запись вида "{{location.x}}, {{location.y}}", например, "600, 350"
-var offerPrices = getRandomNumber(999, 1000000); //число, случайная цена от 1000 до 1 000 000
-var offerTypes = ['palace', 'flat', 'house', 'bungalo'];
-var offerRooms = getRandomNumber(0, 5);
-var offerGuests = getRandomNumber(0, 15); //число, случайное количество гостей, которое можно разместить
-var offerChecks = ['12:00', '13:00', '14:00'];
+var getRandomArray = function() {
+
+};
+
+var getShuffledArray = function {
+
+};
+
+
+var NUMBER_OF_ADVERTS = 8;
+var OFFER_TITLES = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде']; //Значения не должны повторяться.
+var OFFER_TYPES = ['palace', 'flat', 'house', 'bungalo'];
+var OFFER_CHECKS = ['12:00', '13:00', '14:00'];
 //var offerCheckOuts = ['12:00', '13:00', '14:00'];
-var offerFeatures = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner']; //массив строк случайной длины из предложенных
-var offerDescription = '';
-var offerPhotos = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
-var locationPosition = {}; // "location": {
-                           // "x": случайное число, координата x метки на карте от 300 до 900,
-                          // "y": случайное число, координата y метки на карте от 130 до 630
+var OFFER_FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner']; //массив строк случайной длины из предложенных
+var OFFER_PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 
-
+var getSimilarOffer = function() {
+  var locationX = getRandomNumber(300, 900);
+  var locationY = getRandomNumber(130, 630);
+  var advert = {
+    author: {
+      avatar: 'img/avatars/user' + '0' + getRandomNumber(0, 8) + '.png'
+    },
+    offer: {
+    //  title: getUniqueArrayElement(OFFER_TITLES),
+      address: locationX + ', ' + locationY,
+      price: getRandomNumber(1000, 1000000),
+      type: getRandomArrayElement(OFFER_TYPES),
+      rooms: getRandomNumber(1, 5),
+      guests: getRandomNumber(1, 10),
+      checkin: getRandomArrayElement(OFFER_CHECKS),
+      checkout: getRandomArrayElement(OFFER_CHECKS),
+    //  features: getRandomArray(OFFER_FEATURES),
+      description: '',
+    //  photos: getShuffledArray(OFFER_PHOTOS)
+    },
+    location: {
+      x: locationX,
+      y: locationY
+    }
+  };
+  return advert;
+};
+console.log(getSimilarOffer());
 //Создайте массив, состоящий из 8 сгенерированных JS объектов, которые будут описывать похожие объявления неподалёку.
 /*
-var getSimilarOffer = function (arrAvatars, arrTitles, arrTypes, arrRooms, arrGuests, arrCheckIns, arrChecks, arrFeatures, arrPhotos) {
-  var randomName = getRandomArrowElement(arrNames) + ' ' + getRandomArrowElement(arrSurnames);
-  var randomCoat = getRandomArrowElement(arrCoats);
-  var randomEyes = getRandomArrowElement(arrEyes);
+
+/*function generateArrayRandomNumber (min, max) {
+02
+    var totalNumbers        = max - min + 1,
+03
+        arrayTotalNumbers   = [],
+04
+        arrayRandomNumbers  = [],
+05
+        tempRandomNumber;
+06
+
+07
+    while (totalNumbers--) {
+08
+        arrayTotalNumbers.push(totalNumbers + min);
+09
+    }
+10
+
+11
+    while (arrayTotalNumbers.length) {
+12
+        tempRandomNumber = Math.round(Math.random() * (arrayTotalNumbers.length - 1));
+13
+        arrayRandomNumbers.push(arrayTotalNumbers[tempRandomNumber]);
+14
+        arrayTotalNumbers.splice(tempRandomNumber, 1);
+15
+    }
+16
+
+17
+    return arrayRandomNumbers;
+18
+}
+19
+
+20
+//  Нужно учесть что в диапазоне участвуют и минимальное и максимальное число
+21
+//  тоесть если задать (0, 100) то на выходе получим массив из 101-го числа
+22
+//  от 1 до 100 и плюс число 0
+23
+
+24
+alert(generateArrayRandomNumber(45, 67));
+ */
+/*var getSimilarOffer = function (arrAvatars, arrTitles, arrTypes, arrRooms, arrGuests, arrCheckIns, arrChecks, arrFeatures, arrPhotos) {
+  var randomName = getRandomArrayElement(arrNames) + ' ' + getRandomArrayElement(arrSurnames);
+  var randomCoat = getRandomArrayElement(arrCoats);
+  var randomEyes = getRandomArrayElement(arrEyes);
   var wizardsImage = {};
 
   wizardsImage.name = randomName;
@@ -68,15 +151,15 @@ var similarWizardTemplate = document.querySelector('#similar-wizard-template')
     .querySelector('.setup-similar-item');
 
 
-var getRandomArrowElement = function (arr) {
+var getRandomArrayElement = function (arr) {
   var randomElementIndex = Math.floor(Math.random() * arr.length);
   return arr[randomElementIndex];
 };
 
 var getWizardsImage = function (arrNames, arrSurnames, arrCoats, arrEyes) {
-  var randomName = getRandomArrowElement(arrNames) + ' ' + getRandomArrowElement(arrSurnames);
-  var randomCoat = getRandomArrowElement(arrCoats);
-  var randomEyes = getRandomArrowElement(arrEyes);
+  var randomName = getRandomArrayElement(arrNames) + ' ' + getRandomArrayElement(arrSurnames);
+  var randomCoat = getRandomArrayElement(arrCoats);
+  var randomEyes = getRandomArrayElement(arrEyes);
   var wizardsImage = {};
 
   wizardsImage.name = randomName;
