f57351601db695a7a20f90971dd71dd3acc50adb
shantypower <shantypower4891@yandex.ru>
2018-07-12 14:44:57 +1000

    исправлена центровка пина

--- js/pin.js
+++ js/pin.js
@@ -5,7 +5,7 @@
     var mapPinTemplate = document.querySelector('#map__card').content.querySelector('.map__pin');
     var mapPin = mapPinTemplate.cloneNode(true);
     var pinIcon = mapPin.children[0];
-    mapPin.style = 'left: ' + (arrAdverts.location.x + window.constants.PIN_WIDTH / 2) + 'px; top: ' + (arrAdverts.location.y + window.constants.PIN_HEIGHT) + 'px';
+    mapPin.style = 'left: ' + (arrAdverts.location.x - window.constants.PIN_WIDTH / 2) + 'px; top: ' + (arrAdverts.location.y + window.constants.PIN_HEIGHT) + 'px';
     pinIcon.src = arrAdverts.author.avatar;
     pinIcon.alt = arrAdverts.offer.title;
     return mapPin;
@@ -24,7 +24,7 @@
     if (mapPinActive) {
       mapPinActive.classList.remove('map__pin--active');
     }
-  }
+  };
 
   window.pin = {
     createMapPin: createMapPin,
